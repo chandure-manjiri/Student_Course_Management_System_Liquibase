e6643f433c49725608d7be331cf189ef2bf3a17f
manjiri-chandure <chanduremanjiri@gmail.com>
2024-01-30 11:42:16 +0530

    dto & services folder get added from dto-validation branch to feature/mapstruct branch

---
+++ demo/src/main/java/StudentCourseLiquiBase/demo/Dto/CourseDTO.java
@@ -0,0 +1,18 @@
+package StudentCourseLiquiBase.demo.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseDTO { // for responce purpose
+
+    private Integer id;
+    private String name;
+}

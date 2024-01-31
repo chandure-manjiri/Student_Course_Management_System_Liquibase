bf8a2fdf6ebfdf5f98310a2dd2f482f72ea22c0e
manjiri-chandure <chanduremanjiri@gmail.com>
2024-01-31 16:14:37 +0530

    studentNamedto get modified properlly, and all urls runs succesfully

--- demo/src/main/java/StudentCourseLiquiBase/demo/Dto/StudentNameDto.java
+++ demo/src/main/java/StudentCourseLiquiBase/demo/Dto/StudentNameDto.java
@@ -10,6 +10,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class StudentNameDto {
-    String firstName;
-    String lastName;
+    private Integer id;
+    private String firstName;
+    private String lastName;
 }
--- demo/src/main/java/StudentCourseLiquiBase/demo/MapStruct/StudentMapper.java
+++ demo/src/main/java/StudentCourseLiquiBase/demo/MapStruct/StudentMapper.java
@@ -12,8 +12,8 @@ import java.util.Set;
 
 @Mapper(componentModel = "spring", uses = CourseMapper.class)
 public interface StudentMapper {
-   // @Mapping(source = "firstName", target = "firstName")
     @Mapping(source = "lastname", target = "lastName")
+    @Mapping(source = "id", target = "id")
     StudentNameDto convertToStudentNameDto(Student student);
 //    @Mapping(source = "student", target = "studentName")
 
